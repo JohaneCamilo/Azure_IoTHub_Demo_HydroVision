732d1324fcdc7906221c2f01e92824b1513ea7f5
JohaneCamilo <71487464+JohaneCamilo@users.noreply.github.com>
2023-01-17 22:52:06 +0100

    Add files via upload
    Adding the code that constitutes the sensor, an Arduino Uno with a DHT11 to act as a temperature and humidity sensor, as well as the code that would collect and send the data to the cloud database in a JSON file where it can then be extracted and visualized as needed.

---
+++ HydroVision_TemperatureHumiditySensor/Device.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HydroVision_TemperatureHumiditySensor.Models
+{
+    class Device
+    {
+        public string DeviceId { get; set; }
+        public int TimeSent { get; set; }
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+    }
+}
---
+++ HydroVision_TemperatureHumiditySensor/SqlQuery_1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE DeviceMessages
+(
+    Id bigint not null identity(1,1) primary key,
+    DeviceId varchar(50) not null,
+    TimeSent int not null,
+    Temperature float not null,
+    Humidity float not null,
+)
+
+DROP TABLE DeviceMessages;
